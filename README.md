c6c52ff76d873340fb3dbbfbfc6a58e0ff6fa0a0
Vinh Nguyen <nguyenv2@outlook.com>
2019-04-30 10:48:57 -0700

    pre merge commit

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # lab-06-repository
 
-## 1: Repositiory Setup
+## 1: Repository Setup
+
+    Estimate of time needed to complete: 10
+
+    Start time: 0850
+
+    Finish time: 0900
+
+    Actual time needed to complete: 10
+
+## 1: Weather
 
     Estimate of time needed to complete: 10
 
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 'use strict';
+
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
-app.get('/',function(request,response){
-  response.status(200).send('Connected!');
-});
+app.use(express.static('./data'));
+app.use(cors());
+
+app .get('/', getLocation);
 
 app.use('*', (request, response) => response.send('Route doesn\'t exist!'));
-app.listen(PORT, () => {
+
+app. listen (PORT, () => {
   console.log(`Listen on port: ${PORT}`);
 });
+
+function getLocation(req, res) {
+  console.log('eyehd');
+  const location = require('./data/geo.json');
+  res.send(location);
+}
+
+function Location() {
+  this.search_query = "seattle";
+  this.formatted_query = "Seattle, WA, USA";
+  this.latitude = 47.606210;
+  this.longitude = -122.332071;
+}
